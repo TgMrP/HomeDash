87e5f6e69e35e87012596237ec8cce0628db6dd3
Ran Tayar <tgmrp.dev@gmail.com>
2022-03-05 10:33:07 +0200

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-exec su-exec $UID:$GID darkhttpd /www-database/ --no-listing --port "$DPORT"
-exec su-exec $UID:$GID darkhttpd /www/ --no-listing --port "$PORT"
+exec su-exec $UID:$GID darkhttpd /www/ --no-listing --port "$PORT" && darkhttpd /www-database/ --no-listing --port "$DPORT"
