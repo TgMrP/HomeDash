c64eac3cd3b19a75b2dbcc9d833dd712dfd8ab32
Ran Tayar <tgmrp.dev@gmail.com>
2022-03-05 10:37:51 +0200

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,60 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+import { ref, onMounted } from "vue";
+
+const data = ref(null);
+const loading = ref(true);
+const error = ref(null);
+
+function fetchData() {
+  loading.value = true;
+  // I prefer to use fetch
+  // you can use use axios as an alternative
+  return fetch("/database/test.json", {
+    method: "get",
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+    .then((res) => {
+      if (!res.ok) {
+        const error = new Error(res.statusText);
+        error.json = res.json();
+        throw error;
+      }
+
+      return res.json();
+    })
+    .then((json) => {
+      // set the response data
+      data.value = json.data;
+    })
+    .catch((err) => {
+      error.value = err;
+      if (err.json) {
+        return err.json.then((json) => {
+          error.value.message = json.message;
+        });
+      }
+    })
+    .then(() => {
+      loading.value = false;
+    });
+}
+
+onMounted(() => {
+  fetchData();
+});
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <ul v-if="!loading && data">
+      {{
+        data
+      }}
+    </ul>
+
+    <p v-if="loading">Still loading..</p>
+    <p v-if="error"></p>
   </main>
 </template>
