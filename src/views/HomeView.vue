cc293266457934b8b612f7837d0e7f0685aebe97
Ran Tayar <tgmrp.dev@gmail.com>
2022-03-05 10:53:52 +0200

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,18 +14,16 @@ async function fetchData() {
   loading.value = false;
 
   data.value = data;
-  console.log(data.value);
 }
 
 onMounted(() => {
   fetchData();
-  console.log(data.value);
 });
 </script>
 
 <template>
   <main>
-    <div v-if="!loading">data: {{ data.test }}</div>
+    <div v-if="!loading && data.test">data: {{ data.test }}</div>
 
     <p v-if="loading">Still loading..</p>
     <p v-if="error"></p>
